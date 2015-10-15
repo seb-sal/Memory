a57addc25ec90a7b2bf3186fa5736ca91ffb3edf
Sebastian Salazar <sebastian@sasa.guru>
2015-10-15 14:49:46 -0700

    Dd-2

--- js/main.js
+++ js/main.js
@@ -1,19 +1,57 @@
+//global variables
+var randomNumbers = [];
+var level;
+var board;
+var lives;
+var score;
+var $lives = $('#lives');
+var $score = $('#score');
+var $cell = $('.cell');
+
 //users need to exit the game
 function getConfirmation() {
 	var message = confirm("Are you sure you want to quit?");
-		if (message === true) {
-			return window.location = "/index.html";
-		}
-		else {
-			return false;
-		}
-
+	if (message === true) {
+		return window.location = "/index.html";
+	} else {
+		return false;
 	}
+
+}
+
+// reset function
+function reset() {
+	board = [
+		[null, null, null, null],	
+		[null, null, null, null],	
+		[null, null, null, null],	
+		[null, null, null, null]	
+	];
+	lives = 3;
+	score = 0;
+}
+
+reset();
+
+function render() {
+	// display lives
+	$lives.html('LIVES: ' + lives.toString())
+	// display score
+	$score.html('SCORE: ' + score.toString())
+	// flash tiles
+	flashTiles();
+
+}
+
+//flashTiles function
+function flashTiles() {
+}
 
 //develop on click actions for Easy button
 $('.footer').click(function(event) {
 	if (event.target === $('#easy-button')[0]) {
 		levelEasy();
+		$('form p').html('&nbsp;');
 	}
 });
 
@@ -21,6 +59,7 @@ $('.footer').click(function(event) {
 $('.footer').click(function(event) {
 	if (event.target === $('#med-button')[0]) {
 		levelMed();
+		$('form p').html('&nbsp;');
 	}
 });
 
@@ -28,17 +67,9 @@ $('.footer').click(function(event) {
 $('.footer').click(function(event) {
 	if (event.target === $('#hard-button')[0]) {
 		levelHard();
+		$('form p').html('&nbsp;');
 	}
 });
-
-//group the variables into an array
-
-var board = [
-			[null, null, null, null],	
-			[null, null, null, null],	
-			[null, null, null, null],	
-			[null, null, null, null]	
-			];
 
 
 //random tile picker
@@ -47,60 +78,74 @@ var randomMoves = function (num) {
 	return numholder;
 }
 
-//global variables
-var randomY; 
-var randomX;
-var computerMove;
+function playGame() {
+	setTiles();
+	render();
+}
+
 
 //get one random number
-var computerPlay = function() {
-	  randomY = randomMoves(4);
-	  randomX = randomMoves(4);
-	  
-	  while (board[randomY][randomX] == true) {
-	  	randomY = randomMoves(4);
-	  	randomX = randomMoves(4);
-		}
-	  board[randomY][randomX] = true;
-	  return(computerMove);
+var setTiles = function() {
+	var randomY, randomX;
+	var tiles = 0;
+	while (tiles !== level) {
+		randomY = randomMoves(4);
+		randomX = randomMoves(4);
+	 	while (board[randomY][randomX]) {
+			randomY = randomMoves(4);
+			randomX = randomMoves(4);
+	  	}
+		board[randomY][randomX] = true;
+		
+		// create a div with a class of "tile"
+		// then append to td cell
+		// create a .tile css rule: width & height: 100%
+		// update td style in css to add padding
+		var $tile = $('<div>', {'class': 'tile'});
+			$('.cell').append($tile);
+			$tile.css({'width': '100%', 'height': '100%', 'background-color': '#FF7F35'})
+		
+
+	  	tiles++;
+	}
  }  
 
+$tile.on('click', 'button', function(){
+	
+})
 
 //Develop easy level function
 var levelEasy = function() {
-	for (var i =0; i<=3; i++){
-	   computerPlay();  
-	};
-	boxColors();
+	level = 2;
+	playGame();
 }
+
 
 //Develop medium level function
 var levelMed = function() {
-	for (var i =0; i<=7; i++){
-		computerPlay();
-	};
-	boxColors();
+	level = 4;
+	playGame();
 }
 //levelMed();
 
 
 //Develop hard level function
 var levelHard = function() {
-	for (var i =0; i<=11; i++){
-		computerPlay();
-	};
-	boxColors();
+	level = 6;
+	playGame();
 }
+//levelHard();
 
 
+/*
 //highligh random tiles
 var boxColors = function() {
 	for(var i = 0; i < 4; i++){
 		for (var j = 0; j < 4; j++){
 			if (board[i][j] === true){
-				$('#' + i + ","+ j).addClass('.comp-background');
-				$('#' + i + ","+ j).fadeOut(3000, function() {
-				$('#' + i + ","+ j).removeClass('.comp-background')	
+				$('#' + i + "," + j).addClass('comp-background');
+				$('#' + i + "," + j).fadeOut(3000, function() {
+				$('#' + i + "," + j).removeClass('comp-background')	
 				});
 			}
 		}
